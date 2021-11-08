5b27fce3c70a9709daccf33883fb4c39a315a637
Tim Paine <t.paine154@gmail.com>
2021-11-07 21:53:38 -0500

    add copyright to empty file

---
+++ src/js/utils/index.js
@@ -0,0 +1,8 @@
+/* ***************************************************************************
+ *
+ * Copyright (c) 2021, the tributary.js authors.
+ *
+ * This file is part of the tributary.js library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
